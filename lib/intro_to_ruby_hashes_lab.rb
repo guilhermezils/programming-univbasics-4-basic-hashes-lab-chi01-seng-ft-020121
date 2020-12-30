bfb8a9cde0fbcebb922381082eaa733b88ec033a
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2020-12-30 23:17:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,13 @@
 def new_hash
-  Hash.new
+  hash = {}
+  hash
 end
 
+
+
+
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  {first_car: "volkswagen"}
 end
 
 def pioneer
